c0295367c5d2f3bb15e1ed1ca149b86ed651d8cb
Tom <tom@fourzerofour.pw>
2020-04-23 20:41:32 +1000

    Initial commit

---
+++ zip.js
@@ -0,0 +1,27 @@
+export function* zip(fst, snd) {
+
+  const fstIterable = fst[Symbol.iterator]();
+  const sndIterable = snd[Symbol.iterator]();
+
+  let isFirstDone = false;
+  let isSecondDone = false;
+  while (!isFirstDone && !isSecondDone) {
+
+    const a = isFirstDone ? null : fstIterable.next();
+    isFirstDone |= a && a.done;
+
+    const b = isSecondDone ? null : sndIterable.next();
+    isSecondDone |= b && b.done;
+
+    if (isFirstDone && isSecondDone) {
+      break;
+    }
+
+    yield [
+      a && a.value,
+      b && b.value
+    ];
+
+  }
+
+}
